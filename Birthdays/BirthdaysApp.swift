ae83f3f396dbf71fb791589a5bf2ce53e91203ab
Mohit Arora <me@aboutmohit.com>
2024-04-01 10:41:53 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Mohit Arora on 2024-04-01.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
